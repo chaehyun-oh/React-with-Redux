1edd237f6ae11ef6d8ca171c60ccbfb70f624f94
chaehyun_oh <ohchaehh@gmail.com>
2023-01-17 16:38:16 +0900

    230117 update

--- animals/src/App.js
+++ animals/src/App.js
@@ -1,11 +1,11 @@
 function App() {
-    const handleClick = () => {
-        console.log('Button was clicked!');
-    };
+    // const handleClick = () => {
+    //     console.log('Button was clicked!');
+    // };
 
     return <div>
-        <button onClick={handleClick}>Add Animal</button>
-    </div>
+        <button onClick={() => console.log('Button was clicked!')}>Add Animal</button>
+    </div >
 }
 
 export default App;
