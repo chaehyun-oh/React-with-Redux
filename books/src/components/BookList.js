337411c2182df9f9a65c1beab5d37b55949677f9
chaehyun_oh <ohchaehh@gmail.com>
2023-02-03 22:18:06 +0900

    230203 update

--- books/src/components/BookList.js
+++ books/src/components/BookList.js
@@ -1,11 +1,16 @@
+import { useContext } from 'react';
+import BooksContext from '../context/books';
 import BookShow from './BookShow';
 
 function BookList({ books, onDelete, onEdit }) {
+    const value = useContext(BooksContext);
+
     const renderedBooks = books.map((book) => {
         return <BookShow onEdit={onEdit} key={book.id} book={book} onDelete={onDelete} />
     });
 
     return <div className='book-list' >
+        {value}
         {renderedBooks}
     </div>
 }
