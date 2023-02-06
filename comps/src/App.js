d9a543b11487447103cb6b670c83a9f75f434ad7
chaehyun_oh <ohchaehh@gmail.com>
2023-02-06 22:50:49 +0900

    230206 update

---
+++ comps/src/App.js
@@ -0,0 +1,13 @@
+import Button from "./Button";
+
+function App() {
+    return <div>
+        <div>
+            <Button success rounded outline>click me</Button>
+        </div>
+        <div>
+            <Button>click me2</Button>
+        </div>
+    </div>
+}
+export default App;
