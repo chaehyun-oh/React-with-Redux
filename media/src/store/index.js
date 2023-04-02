c38edf2fe7ce7e36cc358fbf492f3c74bb7c5199
chaehyun_oh <ohchaehh@gmail.com>
2023-04-02 23:41:16 +0900

    230402 update

--- media/src/store/index.js
+++ media/src/store/index.js
@@ -1,12 +1,19 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/dist/query';
+import { albumsApi } from './apis/albumsApi';
 import { usersReducer } from './slices/usersSlice';
 
 export const store = configureStore({
     reducer: {
         users: usersReducer,
+        [albumsApi.reducerPath]: albumsApi.reducer,
     },
+    middleware: (getDefaultMiddleware) => {
+        return getDefaultMiddleware().concat(albumsApi.middleware);
+    }
 });
 
 export * from './thunks/fetchUsers';
 export * from './thunks/addUser';
 export * from './thunks/removeUser'
+export { useFetchAlbumsQuery } from './apis/albumsApi';
