91d4eb9b8615058c8ecdd84dd874596febdab6fd
chaehyun_oh <ohchaehh@gmail.com>
2023-04-09 23:59:17 +0900

    230409 update

--- media/src/components/PhotoListItem.js
+++ media/src/components/PhotoListItem.js
@@ -1,5 +1,22 @@
-function PhotoListItem() {
-    return 'PhotoListItem';
+import { GoTrashcan } from "react-icons/go";
+import { useRemovePhotoMutation } from "../store";
+
+
+function PhotoListItem({ photo }) {
+    const [removePhoto, results] = useRemovePhotoMutation();
+
+    const handleRemovePhoto = () => {
+        removePhoto(photo);
+    }
+
+    return (
+        <div className="relative cursor-pointer m-2" onClick={handleRemovePhoto}>
+            <img className="h-20 w-20" src={photo.url} alt="random photo" />
+            <div className="absolute inset-0 flex items-center justify-center hover:bg-gray-200 opacity-0 hover:opacity-80">
+                <GoTrashcan className="text-3xl" />
+            </div>
+        </div>
+    );
 }
 
 export default PhotoListItem;
--- media/src/store/index.js
+++ media/src/store/index.js
@@ -1,19 +1,28 @@
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/dist/query';
-import { albumsApi } from './apis/albumsApi';
 import { usersReducer } from './slices/usersSlice';
+import { albumsApi } from './apis/albumsApi';
+import { photosApi } from './apis/photosApi';
 
 export const store = configureStore({
     reducer: {
         users: usersReducer,
         [albumsApi.reducerPath]: albumsApi.reducer,
+        [photosApi.reducerPath]: photosApi.reducer,
     },
     middleware: (getDefaultMiddleware) => {
-        return getDefaultMiddleware().concat(albumsApi.middleware);
-    }
+        return getDefaultMiddleware()
+            .concat(albumsApi.middleware)
+            .concat(photosApi.middleware);
+    },
 });
 
 export * from './thunks/fetchUsers';
 export * from './thunks/addUser';
 export * from './thunks/removeUser'
 export { useFetchAlbumsQuery, useAddAlbumMutation, useRemoveAlbumMutation } from './apis/albumsApi';
+export {
+    useFetchPhotosQuery,
+    useAddPhotoMutation,
+    useRemovePhotoMutation
+} from './apis/photosApi';
